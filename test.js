2fdb047529566618b271ede277c8dc05e803bcd1
saddhamtajuddhin.pathan <saddhamtajuddhin.pathan@servicenow.com>
2018-11-12 18:47:57 +0530

    Updated jstest cli engine to run a test only when related file is available

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
-const S = require('string'),
+const fs = require('fs'),
+	S = require('string'),
 	opn = require('opn'),
-	glob = require('glob-utils');
+	glob = require('glob-utils'),
+	pkg = require('./package.json'),
+	createLogger = require('logging');
 
-function getSpecName(sourceFileName) {
+	const logger = createLogger.default('jstest-cli');
+    logger.info('hi');
+
+/*function getSpecName(sourceFileName) {
 	var strS = S(sourceFileName);
 	
 	strS = strS.strip('factory.', 'service.', '.js');
@@ -17,8 +23,29 @@ function getSpecName(sourceFileName) {
 	return strS.s;
 }
 
-console.log(glob.basename('./**/*.js'));
+function getSpecFilePath(sourceFileName) {
+    var specFilePathGlob,
+        specFilePath,
+        specFilePathParts,
+        specFilePaths;
+    
+    specFilePathGlob = pkg.jstestConfig.testDir;
 
+    specFilePathParts = S(sourceFileName).splitRight('.', 1);
+    specFilePathGlob = specFilePathGlob + specFilePathParts[0] + '.spec.' + specFilePathParts[1];
+
+    specFilePaths = glob.expand(specFilePathGlob);
+    if(specFilePaths.length > 0)
+        specFilePath = specFilePaths[0];
+
+    return specFilePath;
+}
+
+var specFilePath = getSpecFilePath('service.pwPlannedTaskRecalculator.js');
+console.log(specFilePath);
+if(fs.existsSync(specFilePath))
+	console.log('Yes, it exists!');
+*/
 /*let specName = getSpecName('factory.pwPlannedTaskRecalculator.js');
 opn('http://localhost:8080/$dhtmlxgantt_test.do?spec=' + specName);
 
